aa488e2f5b7847a6d58fe71c1ac8079d5f0ad283
sanjana <sanjanajoshikpl@gmail.com>
2024-12-01 13:48:09 +0530

    Even / Odd java program

---
+++ Sanju.java
@@ -0,0 +1,40 @@
+class Sanju 
+{
+	public static void main(String[] args) 
+	{
+	/*	System.out.println("Hello Sanjana Here");
+		//int i= 0;
+	for (int i = 11; i <= 20; i++)
+		{
+            System.out.println(i + " Sanjana");
+            //System.out.println(i);
+		}
+		System.out.println("------------------------");
+	for (int j = 1; j <= 10; j++)
+		{
+			System.out.println(10*j);
+		} */
+		
+		System.out.println("Even numbers from 1 to 100");
+		for (int j = 1; j <= 100; j++){
+			if(j % 2 ==0)	{
+				System.out.println(j);
+			}
+	    }
+		
+		System.out.println("Odd numbers from 1 to 100");
+		for (int j = 1; j <= 100; j++){
+			if(j % 2 != 0)	{
+				System.out.println(j);
+			}
+	   
+	   
+	   } 
+	   
+	/*   System.out.println("Tables");
+		   for (int i=1; i<=10 ; i++ )
+		   {
+		        System.out.println(11*i);
+		   } */		
+}
+}
